bac291d7817d8a5f76b8c712e27190dee5e305b8
ojedasodev <98894458+ojedasodev@users.noreply.github.com>
2023-09-21 18:34:28 -0500

    .env messages

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 from dotenv import load_dotenv
 import os
 
+"""
+Set up environment variables in .env file in the root directory
+"""
+
 load_dotenv()
 
 ACCOUNT_USERNAME = os.getenv('ACCOUNT_USERNAME')
