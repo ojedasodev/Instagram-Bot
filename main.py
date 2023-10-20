a09ad3bc4204a02d7df77c9c006bbaeb62545080
ojedasodev <98894458+ojedasodev@users.noreply.github.com>
2023-10-19 22:22:37 -0500

    project layout changes

--- main.py
+++ main.py
@@ -14,10 +14,8 @@ def read_root():
 async def get_followers(ig_account: str):
     BackgroundTasks.add_task(get_followers, ig_account, Bot())
     return {"message": "U will get a notification when the bots ends"}
-    
+
+
 def get_followers(account: str, bot: Bot):
-    followers = [ followers for followers in bot.get_followers()]
+    followers = [followers for followers in bot.get_followers()]
     print(followers)
-
-
-   
--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-# Get the parent directory of the src directory
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-
-# Append the parent directory to the sys.path list
-sys.path.append(parent_dir)
